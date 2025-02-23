1597499ac659feba753e4a2f6252c639baf13bd1
Aiman Ahmed <aimanahmed61@gmail.com>
2025-02-23 16:32:10 +0000

    EDIT: added comments to use_functions.py

--- use_functions.py
+++ use_functions.py
@@ -1,9 +1,13 @@
 import game
+# importing the module - can use all its contents
 
 game.rock_paper_scissors()
+# calling main function, file is directly run (script)
 
 user = game.user_conversion("R")
 print(user)
 
 computer = game.computer_conversion(2)
 print(computer)
+# new variables to test each function
+# functions are reusable
